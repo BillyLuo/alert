3f9b4e37d4016f082c48803f9389bbec67b070b7
will <luotao2013@126.com>
2020-07-31 18:25:41 +0800

    init project

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# alert
-a custom alert tip
+# alert.js
+
+> 一个简单的网页提示，css完全借鉴bootstrap alert
+
+## 如何使用
+
+- 网页中引入js, css
+- 引用示例
+  
+```js
+  Alert.alert({
+    type: 'primary',
+    content: '一个超级简单的提示'
+  })
+```
+or
+```js
+  Alert.success('hello, world', 4000)
+```
+
+
+## api
+
+Alert.alert(option)
+
+option:
+
+|字段名称|类型|说明|
+| -- | -- | -- |
+|type|String|提示类型，备选'primary', 'secondary', 'success', 'danger', 'warning', 'info','error', 'light'|
+|content|String|内容，可以为HTML|
+|duration|Int|出现时间，单位毫秒，默认3000|
+
+example:
+
+```js
+  Alert.alert({
+    type: 'success',
+    content: '操作成功',
+    duration: 4000
+  })
+```
+
+```
+另提供Alert.success(content: String, duration: Int)（上述类型）等函数
+```
+
+## License
+
+MIT
---
+++ alert.js
@@ -0,0 +1,90 @@
+// AMD with global, Node, or global
+;
+(function(root, factory) { // eslint-disable-line no-extra-semi
+	if(typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(function() {
+			// Also create a global in case some scripts
+			// that are loaded still are looking for
+			// a global even when an AMD loader is in use.
+			return(root.Alert = factory());
+		});
+	} else if(typeof exports === 'object' && typeof module === 'object'){
+		// Node. Does not work with strict CommonJS, but
+		// only CommonJS-like enviroments that support module.exports,
+		// like Node.
+		module.exports = factory();
+	} else if(typeof exports === 'object') {
+    exports['Alert'] = factory();
+  } else {
+		// Browser globals (root is self)
+    if(typeof window == 'object') {
+    	root = window
+    }
+		root.Alert = factory();
+	}
+}(this, function() {
+  var types = ['primary', 'secondary', 'success', 'danger', 'warning', 'info','error', 'light']
+  var Alert = {
+    alert: function (option) {
+      var defaultOption = {
+        duration: 3000
+      }
+      if (!/^[1-9]\d*$/.test(option.duration)) {
+        option.duration = 3000
+      }
+      if (typeof option == 'string') {
+        option = {
+          content: option
+        }
+      }
+      option = Object.assign(defaultOption, option)
+      option.removeDuration = option.duration + 1000
+      if (types.indexOf(option.type) == '-1') {
+        option.type = types[0]
+      }
+      if (option.type == 'error') {
+        option.type == 'danger'
+      }
+      var wrapper = document.createElement('div')
+      wrapper.className = 'alert-wrapper'
+      var content = document.createElement('div')
+      var typeClass = 'alert'
+      option.type = option.type ? option.type : 'primary'
+      typeClass += ' alert-' + option.type
+      if (!option.content) return
+      var alerts = document.querySelectorAll('.alert-wrapper')
+      alerts.forEach(item => {
+        if (item) {
+          document.body.removeChild(item)
+        }
+      })
+      content.className = typeClass
+      content.innerHTML = option.content
+      wrapper.appendChild(content)
+      document.body.appendChild(wrapper)
+      setTimeout(() => {
+        wrapper.className = wrapper.className + ' alert-out'
+      }, option.duration);
+      setTimeout(() => {
+        if (wrapper) {
+          try{
+            document.body.removeChild(wrapper)
+          }catch(err) {
+            
+          }
+        }
+      }, option.removeDuration);
+    }
+  }
+  types.forEach(item => {
+    Alert[item] = function (content, duration) {
+      Alert.alert({
+        type: item,
+        content: content,
+        duration: duration
+      })
+    }
+  })
+  return Alert
+}))
